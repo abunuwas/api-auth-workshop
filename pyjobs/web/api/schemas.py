9e735d9ee25f693300ea9151cb6efaa6a11b7c98
Jose Haro <joseharoperalta@gmail.com>
2022-09-14 23:02:20 +0100

    Update pydantic schemas

--- pyjobs/web/api/schemas.py
+++ pyjobs/web/api/schemas.py
@@ -1,9 +1,8 @@
-from datetime import datetime
+from datetime import datetime, date
 from enum import Enum
 from typing import Optional
-from uuid import UUID
 
-from pydantic import BaseModel, confloat, conlist, conint, Extra
+from pydantic import BaseModel, confloat, conlist, conint, Extra, create_model
 
 
 class AmountPerTimeEnum(Enum):
@@ -16,31 +15,12 @@ class AmountPerTimeEnum(Enum):
 class Rate(BaseModel):
     amount: confloat(ge=1)
     amountPerTime: AmountPerTimeEnum
-    currency: str
 
 
 class Location(BaseModel):
     city: str
     state: Optional[str]
     country: str
-
-
-class ContractTypeEnum(Enum):
-    contract = "contract"
-    permanent = "permanent"
-
-
-class GetJobSchema(BaseModel):
-    id: UUID
-    title: str
-    dateListed: datetime
-    rate: Rate
-    benefits: str
-    location: Location
-    hirer: str
-    contractType: ContractTypeEnum
-    skills: conlist(str, min_items=1)
-    liveUntil: datetime
 
     def dict(
         self,
@@ -49,6 +29,27 @@ class GetJobSchema(BaseModel):
     ) -> 'DictStrAny':
         kwargs["exclude_none"] = True
         return super().dict(*args, **kwargs)
+
+
+class ContractTypeEnum(Enum):
+    contract = "contract"
+    permanent = "permanent"
+
+
+class GetJobSchema(BaseModel):
+    id: int
+    title: str
+    dateListed: date
+    rate: Rate
+    benefits: str
+    location: Location
+    hirer: str
+    recruiter: create_model(
+        "Recruiter", name=(str, ...), email=(str, ...)
+    )
+    contractType: ContractTypeEnum
+    skills: conlist(str, min_items=1)
+    liveUntil: datetime
 
 
 class ListJobsSchema(BaseModel):
@@ -60,8 +61,8 @@ class CreateJobSchema(BaseModel):
     title: str
     rate: Rate
     benefits: str
-    location: UUID
-    hirer: UUID
+    location: int
+    hirer: int
     contractType: ContractTypeEnum
     description: str
     skills: conlist(str, min_items=1)
@@ -71,3 +72,58 @@ class CreateJobSchema(BaseModel):
 class SortByEnum(Enum):
     datePosted = "datePosted"
     rate = "rate"
+
+
+class SortByOrder(Enum):
+    ascending = "ascending"
+    descending = "descending"
+
+
+class GetSkill(BaseModel):
+    id: int
+    name: str
+
+
+class ListSkills(BaseModel):
+    skills: list[GetSkill]
+    pages: int
+
+
+class GetLocation(BaseModel):
+    id: int
+    city: str
+    state: Optional[str]
+    country: str
+
+    def dict(
+        self,
+        *args,
+        **kwargs
+    ) -> 'DictStrAny':
+        kwargs["exclude_none"] = True
+        return super().dict(*args, **kwargs)
+
+
+class ListLocations(BaseModel):
+    locations: list[GetLocation]
+    pages: int
+
+
+class CreateJobApplication(BaseModel):
+    job_id: int
+
+
+class Candidate(BaseModel):
+    id: int
+
+
+class GetJobApplication(BaseModel):
+    id: int
+    job: GetJobSchema
+    applied: date
+    cancelled: bool
+
+
+class ListJobApplications(BaseModel):
+    applications: list[GetJobApplication]
+    pages: int
