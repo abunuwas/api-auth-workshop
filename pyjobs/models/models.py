a0e333cdd407014f868ae820b340071b35f5abc4
Jose Haro <joseharoperalta@gmail.com>
2022-09-14 23:01:49 +0100

    Update models and regenerate migrations

--- pyjobs/models/models.py
+++ pyjobs/models/models.py
@@ -1,10 +1,17 @@
 from datetime import datetime
 
-from sqlalchemy import Column, String, Integer, ForeignKey, Date, DateTime, Float, UniqueConstraint
+from sqlalchemy import Column, String, Integer, ForeignKey, Date, DateTime, Float, UniqueConstraint, Boolean
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.testing.schema import Table
 
-Base = declarative_base()
+
+class Base:
+    id = Column(Integer, primary_key=True)
+    created = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated = Column(DateTime, nullable=False, default=datetime.utcnow)
+
+
+Base = declarative_base(cls=Base)
 
 
 skills_job_association = Table(
@@ -12,8 +19,6 @@ skills_job_association = Table(
     Base.metadata,
     Column("skill", ForeignKey("skill.id"), nullable=False),
     Column("job", ForeignKey("job.id"), nullable=False),
-    Column("created", DateTime, nullable=False, default=datetime.utcnow),
-    Column("updated", DateTime, nullable=False, default=datetime.utcnow),
 )
 
 
@@ -23,8 +28,6 @@ skills_candidate_association = Table(
     Column("skill", ForeignKey("skill.id"), nullable=False),
     Column("candidate", ForeignKey("candidate.id"), nullable=False),
     Column("years_experience", type_=Integer),
-    Column("created", DateTime, nullable=False, default=datetime.utcnow),
-    Column("updated", DateTime, nullable=False, default=datetime.utcnow),
 )
 
 
@@ -34,13 +37,17 @@ class Location(Base):
         UniqueConstraint("city", "country", name="city_country_constraint"),
     )
 
-    id = Column(Integer, primary_key=True)
-    created = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated = Column(DateTime, nullable=False, default=datetime.utcnow)
-
     city = Column(String, nullable=False)
     state = Column(String)
     country = Column(String, nullable=False)
+
+    def dict(self):
+        return {
+            "id": self.id,
+            "city": self.city,
+            "state": self.state,
+            "country": self.country,
+        }
 
     def __str__(self):
         return (
@@ -49,17 +56,31 @@ class Location(Base):
         )
 
 
+class Skill(Base):
+    __tablename__ = "skill"
+    __table_args__ = (
+        UniqueConstraint("name", name="skill_name_constraint"),
+    )
+
+    name = Column(String, nullable=False)
+
+    def dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+    def __str__(self):
+        return (
+            f"<Skill: id={self.id}, created={self.created}, updated={self.updated}, name=self.{self.name}>"
+        )
+
+
 class Recruiter(Base):
     __tablename__ = "recruiter"
 
-    id = Column(Integer, primary_key=True)
-    created = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated = Column(DateTime, nullable=False, default=datetime.utcnow)
-
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     email = Column(String, nullable=False)
-    contact_number = Column(String)
     hirer_id = Column(Integer, ForeignKey("hirer.id"), nullable=False)
 
     hirer = relationship("Hirer", foreign_keys=[hirer_id])
@@ -75,12 +96,7 @@ class Recruiter(Base):
 class Hirer(Base):
     __tablename__ = "hirer"
 
-    id = Column(Integer, primary_key=True)
-    created = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated = Column(DateTime, nullable=False, default=datetime.utcnow)
-
     name = Column(String, nullable=False)
-
     jobs = relationship("Job", back_populates="hirer")
 
     def __str__(self):
@@ -90,86 +106,62 @@ class Hirer(Base):
         )
 
 
-class Skill(Base):
-    __tablename__ = "skill"
-    __table_args__ = (
-        UniqueConstraint("name", name="skill_name_constraint"),
-    )
-
-    id = Column(Integer, primary_key=True)
-    created = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated = Column(DateTime, nullable=False, default=datetime.utcnow)
-
-    name = Column(String, nullable=False)
-
-    def __str__(self):
-        return (
-            f"<Skill: id={self.id}, created={self.created}, updated={self.updated}, name=self.{self.name}>"
-        )
-
-
-class Currency(Base):
-    __tablename__ = "currency"
-    __table_args__ = (
-        UniqueConstraint("name", "symbol", name="currency_name_symbol_constraint"),
-    )
-
-    id = Column(Integer, primary_key=True)
-    created = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated = Column(DateTime, nullable=False, default=datetime.utcnow)
-
-    name = Column(String, nullable=False)
-    symbol = Column(String, nullable=False)
-
-    def __str__(self):
-        return (
-            f"<Currency: id={self.id}, created={self.created}, updated={self.updated},"
-            f"name={self.name}, symbol={self.symbol}>"
-        )
-
-
 class Job(Base):
     __tablename__ = "job"
-
-    id = Column(Integer, primary_key=True)
-    created = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     hirer_id = Column(Integer, ForeignKey("hirer.id"), nullable=False)
     recruiter_id = Column(Integer, ForeignKey("recruiter.id"), nullable=False)
 
     title = Column(String, nullable=False)
     rate = Column(Float, nullable=False)
-    rate_currency_id = Column(Integer, ForeignKey("currency.id"), nullable=False)
     rate_per_time_unit = Column(String, nullable=False)
     rate_annualized = Column(Float, nullable=False)
-
-    learning_budget = Column(Float)
-    learning_budget_currency_id = Column(Integer, ForeignKey("currency.id"))
     benefits = Column(String)
-
     contract_type = Column(String, nullable=False)
     description = Column(String, nullable=False)
     date_posted = Column(Date, nullable=False, default=lambda: datetime.utcnow().date())
     live_until = Column(DateTime, nullable=False)
     location_id = Column(Integer, ForeignKey("location.id"), nullable=False)
+    visible = Column(Boolean, nullable=False, default=True)
 
     applications = relationship("JobApplication", back_populates="job_listing")
     hirer = relationship("Hirer", back_populates="jobs")
     recruiter = relationship("Recruiter", foreign_keys=[recruiter_id])
-    rate_currency = relationship("Currency", foreign_keys=[rate_currency_id])
-    learning_budget_currency = relationship("Currency", foreign_keys=[learning_budget_currency_id])
     location = relationship("Location", foreign_keys=[location_id])
     skills = relationship("Skill", secondary=skills_job_association)
+
+    def dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "rate": {
+                "amount": self.rate,
+                "amountPerTime": self.rate_per_time_unit,
+            },
+            "benefits": self.benefits or "",
+            "contractType": self.contract_type,
+            "description": self.description,
+            "dateListed": self.date_posted,
+            "liveUntil": self.live_until,
+            "location": {
+                "city": self.location.city,
+                "state": self.location.state,
+                "country": self.location.country,
+            },
+            "hirer": self.hirer.name,
+            "recruiter": {
+                "name": self.recruiter.name,
+                "email": self.recruiter.email,
+            },
+            "skills": [skill.name for skill in self.skills]
+        }
 
     def __str__(self):
         return (
             f"<Job: id={self.id}, created={self.created}, updated={self.updated},"
             f"hirer_id={self.hirer_id}, recruiter_id={self.recruiter_id}, title={self.title},"
-            f"rate={self.rate}, rate_currency_id={self.rate_currency_id}, "
-            f"rate_per_time_unit={self.rate_per_time_unit}, rate_annualized={self.rate_annualized},"
-            f"learning_budget={self.learning_budget}, learning_budget_currency_id={self.learning_budget_currency_id},"
-            f"benefits={self.benefits}, contract_type={self.contract_type}, description={self.description},"
+            f"rate={self.rate}, rate_per_time_unit={self.rate_per_time_unit}, rate_annualized={self.rate_annualized},"
+            f"benefits={self.benefits}, contract_type={self.contract_type}, description={self.description}, "
             f"date_posted={self.date_posted}, live_until={self.live_until}, location_id={self.location_id}>"
         )
 
@@ -177,14 +169,8 @@ class Job(Base):
 class Candidate(Base):
     __tablename__ = "candidate"
 
-    id = Column(Integer, primary_key=True)
-    created = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated = Column(DateTime, nullable=False, default=datetime.utcnow)
-
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     email = Column(String, nullable=False)
-    contact_number = Column(String)
     cv = Column(String)
     location_id = Column(Integer, ForeignKey("location.id"), nullable=False)
 
@@ -203,15 +189,20 @@ class Candidate(Base):
 class JobApplication(Base):
     __tablename__ = "job_application"
 
-    id = Column(Integer, primary_key=True)
-    created = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated = Column(DateTime, nullable=False, default=datetime.utcnow)
-
     candidate_id = Column(Integer, ForeignKey("candidate.id"), nullable=False)
     job_listing_id = Column(Integer, ForeignKey("job.id"), nullable=False)
+    cancelled = Column(Boolean, nullable=False, default=False)
 
     job_listing = relationship("Job", back_populates="applications")
     candidate = relationship("Candidate", back_populates="applications")
+
+    def dict(self):
+        return {
+            "id": self.id,
+            "applied": self.created,
+            "cancelled": self.cancelled,
+            "job": self.job_listing.dict()
+        }
 
     def __str__(self):
         return (
